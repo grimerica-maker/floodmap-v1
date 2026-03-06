1891f4111e4e023842439933f0344c21ce610be6
grimerica-maker <grimerica@gmail.com>
2026-03-06 16:08:08 -0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Floodmap V1",
+  description: "Flood map app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
