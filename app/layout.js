b09b4f11d8c7e03a056312ee5722657c5133336f
grimerica-maker <grimerica@gmail.com>
2026-03-06 16:22:13 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Floodmap V1",
+  description: "Flood map app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
