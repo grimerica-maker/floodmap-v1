c0e3c0f4e16396a375ad75a84ba73773dd5c25c6
grimerica-maker <grimerica@gmail.com>
2026-03-06 16:33:27 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,27 +1,75 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import mapboxgl from "mapbox-gl"
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN
 
 export default function HomePage() {
+
   const mapContainer = useRef(null)
+  const mapRef = useRef(null)
+
+  const [seaLevel, setSeaLevel] = useState(0)
 
   useEffect(() => {
+
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v12",
-      center: [-95.3698, 29.7604],
-      zoom: 5
+      center: [-95, 30],
+      zoom: 3,
+      projection: "globe"
     })
 
+    map.addControl(new mapboxgl.NavigationControl())
+
+    map.on("style.load", () => {
+
+      map.setFog({})
+
+      map.addSource("flood", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.terrain-rgb"
+      })
+
+    })
+
+    mapRef.current = map
+
     return () => map.remove()
+
   }, [])
 
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <div ref={mapContainer} style={{ width: "100%", height: "100%" }} />
+    <div style={{width:"100%",height:"100vh"}}>
+
+      <div
+        ref={mapContainer}
+        style={{width:"100%",height:"100%"}}
+      />
+
+      <div style={{
+        position:"absolute",
+        top:20,
+        left:20,
+        background:"white",
+        padding:12,
+        borderRadius:8
+      }}>
+
+        <div>Sea Level: {seaLevel} m</div>
+
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={seaLevel}
+          onChange={(e)=>setSeaLevel(e.target.value)}
+        />
+
+      </div>
+
     </div>
   )
 }
